4589ef4a477e1448c85785f9932cad3a71348624
Blam25 <adrian.martvall@gmail.com>
2024-11-05 22:09:51 +0100

    React och tone bas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as Tone from "tone";
 
-function App() {
+const test = () => {
+  console.log("yo");
+  synth.triggerAttackRelease("C4", "8n");
+};
+
+const synth = new Tone.Synth().toDestination();
+
+function TestButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button variant="outlined" onClick={test}>
+      Outlined
+    </button>
   );
 }
 
-export default App;
+export default TestButton;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
